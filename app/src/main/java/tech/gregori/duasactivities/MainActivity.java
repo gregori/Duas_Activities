2d8f5840d4714ffefa7c8c1868f5ed7c4f33f8ed
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2021-05-03 21:58:29 -0300

    Etapa 1.5 - adicionar segunda Activity

--- app/src/main/java/tech/gregori/duasactivities/MainActivity.java
+++ app/src/main/java/tech/gregori/duasactivities/MainActivity.java
@@ -2,6 +2,7 @@ package tech.gregori.duasactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(TAG, "launchSecondActivity: Botão pressionadi!");
+        Log.d(TAG, "launchSecondActivity: Botão pressionado!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
